e61a7c4dc8a29507ef0543d72c379832d37b3e98
crinq <crinq@crinq.de>
2020-12-05 01:58:56 +0100

    merge curpid

--- comps/curpid/curpid.c
+++ comps/curpid/curpid.c
@@ -10,34 +10,43 @@ HAL_COMP(curpid);
 
 // enable
 HAL_PIN(en);
-HAL_PIN(cmd_mode);
+HAL_PIN(cmd_mode); // volt, current
+HAL_PIN(mode); // dq transformation
 
 // current command
 HAL_PIN(id_cmd);
 HAL_PIN(iq_cmd);
 
 // current feedback
-HAL_PIN(id_fb);
-HAL_PIN(iq_fb);
-
-// HAL_PIN(ac_current);
+HAL_PIN(iu);
+HAL_PIN(iv);
+HAL_PIN(iw);
+HAL_PIN(ia);
+HAL_PIN(ib);
+HAL_PIN(iy);
+HAL_PIN(id);
+HAL_PIN(iq);
 
 // voltage output
 HAL_PIN(ud);
 HAL_PIN(uq);
+HAL_PIN(ua);
+HAL_PIN(ub);
+HAL_PIN(uu);
+HAL_PIN(uv);
+HAL_PIN(uw);
 
 // maximum output current and voltage
 HAL_PIN(max_cur);
-HAL_PIN(pwm_volt);
+HAL_PIN(max_volt);
 
-// d, q resistance and inductance
+//resistance, d/q inductance, pm flux
 HAL_PIN(r);
 HAL_PIN(ld);
 HAL_PIN(lq);
-
-// torque constant
 HAL_PIN(psi);
 
+// gains
 HAL_PIN(ff);  // r feed forward
 HAL_PIN(kp);
 HAL_PIN(ki);
@@ -45,128 +54,216 @@ HAL_PIN(ksp); // predictor
 HAL_PIN(kind);  // bemf feed forward
 HAL_PIN(kci);
 
+// flux gains
+HAL_PIN(flp);
+HAL_PIN(kfp);
+HAL_PIN(flux);
+HAL_PIN(ea);
+HAL_PIN(eb);
+HAL_PIN(fa);
+HAL_PIN(fb);
+HAL_PIN(min_flux);
+
 HAL_PIN(scale);
 
+HAL_PIN(pos_in);  // position input
+HAL_PIN(pos_out);  // position output
 HAL_PIN(vel);  // velocity input
 
 // current error outputs
 HAL_PIN(id_error);
 HAL_PIN(iq_error);
 
-struct curpid_ctx_t {
-  float id_error_sum;
-  float iq_error_sum;
-};
+HAL_PIN(id_error_sum);
+HAL_PIN(iq_error_sum);
+
+HAL_PIN(sin);
+HAL_PIN(cos);
 
 static void nrt_init(void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
   // struct curpid_ctx_t * ctx = (struct curpid_ctx_t *)ctx_ptr;
   struct curpid_pin_ctx_t *pins = (struct curpid_pin_ctx_t *)pin_ptr;
 
-  PIN(r)  = 0.5;
-  PIN(ld)  = 0.01;
-  PIN(lq)  = 0.01;
+  PIN(r)  = 0.05;
+  PIN(ld)  = 0.00006;
+  PIN(lq)  = 0.00006;
   PIN(psi) = 0.05;
   PIN(kp)  = 0.1;
   PIN(ki)  = 0.005;
   PIN(kci)  = 500.0;
   PIN(ksp)  = 1.0;
   PIN(scale) = 1.0;
+
+  PIN(flp) = 0.99;
+  PIN(min_flux) = 0.005;
+  PIN(kfp) = 0.01;
 }
 
 static void rt_func(float period, void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
-  struct curpid_ctx_t *ctx      = (struct curpid_ctx_t *)ctx_ptr;
+  // struct curpid_ctx_t *ctx      = (struct curpid_ctx_t *)ctx_ptr;
   struct curpid_pin_ctx_t *pins = (struct curpid_pin_ctx_t *)pin_ptr;
 
-  float r = MAX(PIN(r), 0.1);
-  float ld = MAX(PIN(ld), 0.001);
-  float lq = MAX(PIN(lq), 0.001);
+  // clark transformation
+  switch((int)PIN(mode)) {
+    case PHASE_90_3PH:  // 90°
+      PIN(ia) = PIN(iu) - PIN(iv);
+      PIN(ib) = PIN(iw) - PIN(iv);
+      PIN(iy) = PIN(iu) / 3.0 + PIN(iv) / 3.0 + PIN(iw) / 3.0;
+    break;
 
-  float ff   = PIN(ff);
-  float kind = PIN(kind);
-  float kpd  = ld * PIN(kp) / period / 2.0;
-  float kid  = r * PIN(ki) / ld;
-  float kpq  = lq * PIN(kp) / period / 2.0;
-  float kiq  = r * PIN(ki) / lq;
+    case PHASE_120_3PH:  // 120°
+      PIN(ia) = PIN(iu) * 2.0 / 3.0 - PIN(iv) / 3.0 - PIN(iw) / 3.0;
+      PIN(ib) = PIN(iv) / M_SQRT3 - PIN(iw) / M_SQRT3;
+      PIN(iy) = PIN(iu) / 3.0 + PIN(iv) / 3.0 + PIN(iw) / 3.0;
+    break;
 
-  float max_cur = MAX(PIN(max_cur), 0.01);
+    case PHASE_180_2PH:  // 180°
+      PIN(ia) = 0;
+      PIN(ib) = (PIN(iu) - PIN(iw)) / 2.0;
+      PIN(iy) = (PIN(iu) + PIN(iw)) / 2.0;
+      break;
+
+    case PHASE_180_3PH:  // 180°
+      PIN(ia) = PIN(iv);
+      PIN(ib) = (PIN(iu) - PIN(iw)) / 2.0;
+      PIN(iy) = (PIN(iu) + PIN(iw)) / 2.0;
+      break;
+
+    default:
+      PIN(ia) = 0.0;
+      PIN(ib) = 0.0;
+      PIN(iy) = 0.0;
+  }
+
+  // flux observer
+  PIN(ea) += (PIN(ua) - PIN(ia) * PIN(r)) * period;
+  PIN(ea) *= PIN(flp);
+  PIN(fa) = PIN(ea) - PIN(ia) * PIN(ld);
+
+  PIN(eb) += (PIN(ub) - PIN(ib) * PIN(r)) * period;
+  PIN(eb) *= PIN(flp);
+  PIN(fb) = PIN(eb) - PIN(ib) * PIN(ld);
+
+  PIN(flux) = sqrtf(PIN(fa) * PIN(fa) + PIN(fb) * PIN(fb));
+
+  if(PIN(flux) >= PIN(min_flux)){
+    PIN(pos_out)  = atan2f(PIN(fb), PIN(fa));
+  }
+
+  float pos_error = minus(PIN(pos_in), PIN(pos_out));
+  PIN(pos_out) = mod(PIN(pos_out) + pos_error * PIN(kfp) * period);
+
+  
+  //park transformation
+  sincos_fast(PIN(pos_out), &PIN(sin), &PIN(sin));
+  PIN(id) = PIN(ia) * PIN(cos) + PIN(ib) * PIN(sin);
+  PIN(iq) = -PIN(ia) * PIN(sin) + PIN(ib) * PIN(cos);
+
+  // curpid
+  float kpd  = PIN(ld) * PIN(kp) / period / 2.0;
+  float kid  = PIN(r) * PIN(ki);
+  float kpq  = PIN(lq) * PIN(kp) / period / 2.0;
+  float kiq  = PIN(r) * PIN(ki);
+
   float idc     = PIN(id_cmd);
   float iqc     = PIN(iq_cmd);
-
-  float max_volt = PIN(pwm_volt);
-
-  float id = PIN(id_fb);
-  float iq = PIN(iq_fb);
-
-  // float ac_current = id * id + iq * iq;  //sqrtf(id * id + iq * iq);
-  // PIN(ac_current)  = ac_current;
 
   float abscur;
   float absvolt;
 
   if(PIN(cmd_mode) == VOLT_MODE) {
     absvolt = idc * idc + iqc * iqc; // clamp cmd
-    PIN(scale) *= sqrtf(CLAMP(max_volt * max_volt / MAX(absvolt, max_volt * 0.1), 0.0, 1.0));
+    PIN(scale) *= sqrtf(CLAMP(PIN(max_volt) * PIN(max_volt) / MAX(absvolt, PIN(max_volt) * 0.1), 0.0, 1.0));
 
-    abscur = id * id + iq * iq; // clamp over fb
-    PIN(scale) += (max_cur * max_cur - abscur) * PIN(kci) * period;
+    abscur = PIN(id) * PIN(id) + PIN(iq) * PIN(iq); // clamp over fb
+    PIN(scale) += (PIN(max_cur) * PIN(max_cur) - abscur) * PIN(kci) * period;
   }
   else{
     abscur = idc * idc + iqc * iqc; // clamp cmd
-    PIN(scale) = sqrtf(max_cur * max_cur / MAX(abscur, max_cur * 0.1));
+    PIN(scale) = sqrtf(PIN(max_cur) * PIN(max_cur) / MAX(abscur, PIN(max_cur) * 0.1));
   }
   PIN(scale) = CLAMP(PIN(scale), 0.0, 1.0);
   
   idc *= PIN(scale);
   iqc *= PIN(scale);
 
-  float vel   = PIN(vel);
-  float psi_d = ld * id + PIN(psi);
-  float psi_q = lq * iq;
-  float indd  = vel * psi_q;
-  float indq  = vel * psi_d;
+  float psi_d = PIN(ld) * PIN(id) + PIN(psi);
+  float psi_q = PIN(lq) * PIN(iq);
+  float indd  = PIN(vel) * psi_q;
+  float indq  = PIN(vel) * psi_d;
 
   // predictor to cancel pwm delay
-  id += (PIN(ud) - r * id + indd) / ld * period * PIN(ksp);
-  iq += (PIN(uq) - r * iq - indq) / lq * period * PIN(ksp);
+  // id += (PIN(ud) - r * id + indd) / ld * period * PIN(ksp);
+  // iq += (PIN(uq) - r * iq - indq) / lq * period * PIN(ksp);
 
-  float id_error = idc - id;
-  float iq_error = iqc - iq;
+  PIN(id_error) = idc - PIN(id);
+  PIN(iq_error) = iqc - PIN(iq);
 
-  float ud = LIMIT(ff * r * idc - kind * indd + kpd * id_error, max_volt);
-  float uq = LIMIT(ff * r * iqc + kind * indq + kpq * iq_error, max_volt);
+  PIN(ud) = LIMIT(PIN(ff) * PIN(r) * idc - PIN(kind) * indd + kpd * PIN(id_error), PIN(max_volt));
+  PIN(uq) = LIMIT(PIN(ff) * PIN(r) * iqc + PIN(kind) * indq + kpq * PIN(iq_error), PIN(max_volt));
 
-  if(kpd * kid > 0.0 && kpq * kiq > 0.0 && PIN(en) > 0.0) {
-    ctx->id_error_sum = LIMIT(ctx->id_error_sum + kpd * kid * id_error * period, max_volt - ud);
-    ctx->iq_error_sum = LIMIT(ctx->iq_error_sum + kpq * kiq * iq_error * period, max_volt - uq);
+  if(PIN(en) > 0.0){
+    PIN(id_error_sum) = LIMIT(PIN(id_error_sum) + kid * PIN(id_error) * period, PIN(max_volt) - PIN(ud));
+    PIN(iq_error_sum) = LIMIT(PIN(iq_error_sum) + kiq * PIN(iq_error) * period, PIN(max_volt) - PIN(uq));
   }
 
-  ud += ctx->id_error_sum;
-  uq += ctx->iq_error_sum;
+  PIN(ud) += PIN(id_error_sum);
+  PIN(uq) += PIN(iq_error_sum);
 
   if(PIN(cmd_mode) == VOLT_MODE) {
-    ud                = idc;
-    uq                = iqc;
-    ctx->id_error_sum = 0.0;
-    ctx->iq_error_sum = 0.0;
-    id_error          = 0.0;
-    iq_error          = 0.0;
+    PIN(ud)           = idc;
+    PIN(uq)           = iqc;
+    PIN(id_error_sum) = 0.0;
+    PIN(iq_error_sum) = 0.0;
+    PIN(id_error)     = 0.0;
+    PIN(iq_error)     = 0.0;
   }
 
   if(PIN(en) <= 0.0) {
-    ud                = 0.0;
-    uq                = 0.0;
+    PIN(ud)           = 0.0;
+    PIN(uq)           = 0.0;
   }
 
   if(PIN(en) == 0.0){
-    ctx->id_error_sum = 0.0;
-    ctx->iq_error_sum = 0.0;
+    PIN(id_error_sum) = 0.0;
+    PIN(iq_error_sum) = 0.0;
   }
 
-  PIN(ud) = ud;
-  PIN(uq) = uq;
+  //inverse park transformation
+  PIN(ua) = PIN(ud) * PIN(cos) - PIN(uq) * PIN(sin);
+  PIN(ub) = PIN(ud) * PIN(sin) + PIN(uq) * PIN(cos);
 
-  PIN(id_error) = id_error;
-  PIN(iq_error) = iq_error;
+  //inverse clarke transformation
+  switch((int)PIN(mode)) {
+    case PHASE_90_3PH:  // 90°
+      PIN(uu) = PIN(ua);
+      PIN(uv) = 0.0;
+      PIN(uw) = PIN(ub);
+      break;
+
+    case PHASE_120_3PH:  // 120°
+      PIN(uu) = PIN(ua);
+      PIN(uv) = -PIN(ua) / 2.0 + PIN(ub) / 2.0 * M_SQRT3;
+      PIN(uw) = -PIN(ua) / 2.0 - PIN(ub) / 2.0 * M_SQRT3;
+      break;
+
+    case PHASE_180_2PH:  // 180°
+      PIN(uu) = PIN(ub) / 2.0;
+      PIN(uv) = 0.0;
+      PIN(uw) = -PIN(ub) / 2.0;
+      break;
+
+    case PHASE_180_3PH:  // 180°
+      PIN(uu) = PIN(ub) / 2.0;
+      PIN(uv) = PIN(ua);
+      PIN(uw) = -PIN(ub) / 2.0;
+      break;
+
+    default:
+      PIN(uu) = 0.0;
+      PIN(uv) = 0.0;
+      PIN(uw) = 0.0;
+  }
 }
 
 hal_comp_t curpid_comp_struct = {
@@ -179,6 +276,6 @@ hal_comp_t curpid_comp_struct = {
     .frt_start = 0,
     .rt_stop   = 0,
     .frt_stop  = 0,
-    .ctx_size  = sizeof(struct curpid_ctx_t),
+    .ctx_size  = 0,
     .pin_count = sizeof(struct curpid_pin_ctx_t) / sizeof(struct hal_pin_inst_t),
 };
